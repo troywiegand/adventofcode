6aff1921ed0109a0acb7e6562f19fdca1967a621
Troy Wiegand <troy.wiegand@kinneygroup.com>
2022-12-06 12:35:15 -0500

    feat: Solve Day 02 Part Two
    Signed-off-by: Troy Wiegand <troy.wiegand@kinneygroup.com>

--- day02/index.ts
+++ day02/index.ts
@@ -69,3 +69,46 @@ const roundScores = roundActions.map((s) => {
 })
 
 console.log(`Your overal score would be: ${roundScores.reduce((x,y) => x+y,0)}`)
+
+enum RPSOutcomes {
+  Draw=3,
+  Win=6,
+  Lose=0
+}
+
+const ElfStrat = new Map<RPSOptsStrat, number>([
+  [RPSOptsStrat.X, RPSOutcomes.Lose],
+  [RPSOptsStrat.Y, RPSOutcomes.Draw],
+  [RPSOptsStrat.Z, RPSOutcomes.Win],
+]);
+
+const getScoreFromOutcome = (y: RPSOptsStrat) => {
+  return ElfStrat.get(y) || 0;
+}
+
+const getSelectionBasedOnOutcome = (x: RPSOpts, y: RPSOptsStrat) => {
+  const xVal = RPSNormal.get(x) || 0;
+  const yOutcome = ElfStrat.get(y) || 0;
+  if (yOutcome === RPSOutcomes.Draw) {
+    return xVal;
+  } else {
+    const moddedChoice = yOutcome === RPSOutcomes.Win ? (xVal+1)%3 : (xVal-1)%3;
+    return moddedChoice === 0 ? 3 : moddedChoice;
+  }
+  
+}
+
+const part2RoundScores = roundActions.map((s) => {
+  const picks = s.split(' ');
+  if (picks.length !== 2) {
+    return 0;
+  }
+  const opponentPick = RPSOpts[picks[0] as keyof typeof RPSOpts];
+  const myPick = RPSOptsStrat[picks[1] as keyof typeof RPSOptsStrat];
+  const selectionScore = getSelectionBasedOnOutcome(opponentPick, myPick);
+  const matchScore = getScoreFromOutcome(myPick);
+  // console.log(`${JSON.stringify(picks)} -> Opponent picked ${opponentPick} and I want the outcome of ${RPSStrat2.get(myPick)} so I get ${matchScore} and picked ${selectionScore}`)
+  return selectionScore + matchScore;
+})
+
+console.log(`Your overal score using Elf's Strat would be: ${part2RoundScores.reduce((x,y) => x+y,0)}`)
